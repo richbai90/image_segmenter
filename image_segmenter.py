db29b95ae2d89d7d007bb9f1909880a2ffd9f251
Rich Baird <rich@rbaird.me>
2023-06-27 12:06:44 -0600

    add version

--- image_segmenter.py
+++ image_segmenter.py
@@ -7,6 +7,8 @@ date: 2023-06-08
 description: A class for annotating images with bounding boxes using various image processing techniques.
 """
 
+__version__ = '0.1.0'
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
